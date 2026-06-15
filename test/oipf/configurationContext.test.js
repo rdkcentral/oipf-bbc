b30a5f890fb32e08bd96de596b07bc8626a5940f
Paul Hogarth <paul.hogarth@infosys.com>
2026-06-15 11:49:25 +0100

    feat: Add license headers, fix accreditation, and remove example urls until true data format is known. Resolved additional copilot review comments.
    Resolves: RDKDEV-1373

--- test/oipf/configurationContext.test.js
+++ test/oipf/configurationContext.test.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2026 Infosys
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Unit tests for lib/oipf/configurationContext.js
  *
@@ -39,7 +55,7 @@ describe('oipf/configurationContext', () => {
             expect(ctx.getConfiguration()).to.deep.equal({
                 preferedAudioLanguage: 'eng',
                 preferedSubtitleLanguage: 'eng',
-                countryId: 'gbr',
+                countryId: 'GBR',
                 subtitlesEnabled: false,
                 audioDescriptionEnabled: false
             });
@@ -118,7 +134,7 @@ describe('oipf/configurationContext', () => {
             ctx.init();
             await flush();
 
-            expect(ctx.getConfiguration().countryId).to.equal('gbr');
+            expect(ctx.getConfiguration().countryId).to.equal('GBR');
         });
 
         it('keeps default countryId when Firebolt returns an unmapped country', async () => {
@@ -129,7 +145,7 @@ describe('oipf/configurationContext', () => {
             ctx.init();
             await flush();
 
-            expect(ctx.getConfiguration().countryId).to.equal('gbr');
+            expect(ctx.getConfiguration().countryId).to.equal('GBR');
         });
 
         it('keeps default audio language when Firebolt returns []', async () => {
@@ -185,7 +201,7 @@ describe('oipf/configurationContext', () => {
             expect(ctx.getConfiguration()).to.deep.equal({
                 preferedAudioLanguage: 'eng',
                 preferedSubtitleLanguage: 'eng',
-                countryId: 'gbr',
+                countryId: 'GBR',
                 subtitlesEnabled: false,
                 audioDescriptionEnabled: false
             });
